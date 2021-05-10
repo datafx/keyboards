1bf3262878e89f6ff4e0d9a1ee542ca2067e20c3
datafx <2432997+datafx@users.noreply.github.com>
2021-05-10 19:13:54 -0400

    tapdance and keymap updates

--- mechwild/mercutio/rules.mk
+++ mechwild/mercutio/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = USBasp
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -22,3 +22,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
