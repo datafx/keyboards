d0748bd5af6dba72bb21e354673a0e02f39e9dc2
datafx <2432997+datafx@users.noreply.github.com>
2021-05-09 21:44:46 -0400

    more oled updates

--- mechwild/mercutio/keymaps/via/keymap.c
+++ mechwild/mercutio/keymaps/via/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       MO(3),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
       KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
-      KC_LCTL, KC_LALT, KC_LGUI,          MO(1),   MO(2),            KC_SPC,           KC_RGUI, KC_RALT,          KC_RCTL ),
+      KC_LCTL, KC_LGUI, KC_LALT,          MO(1),   MO(2),            KC_SPC,           KC_RALT, KC_RGUI,          KC_RCTL ),
 
   [1] = LAYOUT_all(
                                                                                                                   KC_TRNS,
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
 
   [2] = LAYOUT_all(
-                                                                                                                  KC_TRNS,
+                                                                                                                  AG_TOGG,
           KC_GRV,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
           KC_CAPS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
@@ -105,6 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 default:
                     oled_write_P(PSTR("Active` Layer ?"), false);    // Should never display, here as a catchall                
         }
+        oled_set_cursor(0,1);
+            if (keymap_config.swap_ralt_rgui) {
+              oled_write_P(PSTR("MacOS Mode  "), false);
+            } else {
+              oled_write_P(PSTR("Windows Mode"), false);
+            }
+
+
         led_t led_state = host_keyboard_led_state();
         oled_set_cursor(16,3);
         oled_write_P(led_state.scroll_lock ? PSTR("SCLK") : PSTR("    "), false);
